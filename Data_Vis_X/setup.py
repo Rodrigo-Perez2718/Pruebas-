a74c7dd46141fb1ded4c84ab8ab2ee0cac940efd
Rodrigo-Perez2718 <135919146+Rodrigo-Perez2718@users.noreply.github.com>
2023-07-20 22:54:10 -0400

    Add files via upload

---
+++ Data_Vis_X/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DataVisX',
+    version='0.1.0',
+    description='Descripción de mi paquete',
+    author='Rodigo Pérez - Gabriela Segura - Javier Gallardo - Amirah luna izidine',
+    packages=find_packages(),
+    install_requires=[
+        # Lista de dependencias necesarias para tu paquete (si tienes alguna)
+    ]
+)
